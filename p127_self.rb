ae56e8c2400f8543b6262c58f285eb116a51acb1
IMAZU Asami <neko.daisuki.070109@gmail.com>
2013-04-27 14:41:51 +0900

    Add nested describe

--- p127_self.rb
+++ p127_self.rb
@@ -20,6 +20,35 @@ end
 p "main直下でコールすると -> " + functionn.to_s
 
 describe "Self class" do
+  describe "Nested describe" do
+    p "Nested describe でコールすると -> " + functionn.to_s
+    before:all do
+      p "Nested describe の before でコールすると -> " + functionn.to_s
+    end
+    it "dummy" do
+      true
+    end
+  end
+
+  describe "Nested describe 2" do
+    describe "Nested nested describe" do
+      p "Nested nested describe でコールすると -> " + functionn.to_s
+      before:all do
+        p "Nested nested describe の before でコールすると -> " + functionn.to_s
+      end
+      it "dummy" do
+        true
+      end
+    end
+    p "2番目の Nested describe でコールすると -> " + functionn.to_s
+    before:all do
+      p "2番目の Nested describe の before でコールすると -> " + functionn.to_s
+    end
+    it "dummy" do
+      true
+    end
+  end
+
   before:all do
     p "describeメソッドの中でコールすると -> " + functionn.to_s
   end
